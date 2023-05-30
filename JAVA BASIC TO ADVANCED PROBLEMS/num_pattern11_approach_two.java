271e30b06124c01607e1616accfd98ec7ff93891
vcdhruv <95232452+vcdhruv@users.noreply.github.com>
2023-05-30 22:42:03 +0530

    numeric pattern approach two

---
+++ JAVA BASIC TO ADVANCED PROBLEMS/num_pattern11_approach_two.java
@@ -0,0 +1,10 @@
+public class num_pattern11_approach_two {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.print(j+" "+i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
